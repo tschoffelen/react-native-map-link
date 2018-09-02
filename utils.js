b1d7a3f37480b69e95058734d93b89994cbef524
pilot4u <4d.lerner@gmail.com>
2018-09-03 02:33:43 +0300

    added availableApps caching

--- utils.js
+++ utils.js
@@ -2,27 +2,32 @@
  * React Native Map Link
  */
 
-import { Linking, ActionSheetIOS, Alert } from 'react-native'
+import { Linking, ActionSheetIOS, Alert } from "react-native";
 
-import { prefixes, titles, isIOS } from './constants'
+import { prefixes, titles, isIOS } from "./constants";
 
 /**
  * Get available navigation apps.
  */
+
+var availableApps = null;
+
 export const getAvailableApps = async () => {
-  let availableApps = []
+  if (availableApps) return availableApps;
+  console.log('getting availableApps');
+  availableApps = [];
   for (let app in prefixes) {
     if (!prefixes.hasOwnProperty(app)) {
-      continue
+      continue;
     }
-    let avail = await isAppInstalled(app)
+    let avail = await isAppInstalled(app);
     if (avail) {
-      availableApps.push(app)
+      availableApps.push(app);
     }
   }
 
-  return availableApps
-}
+  return availableApps;
+};
 
 /**
  * Check if a given map app is installed.
@@ -30,18 +35,18 @@ export const getAvailableApps = async () => {
  * @param {string} app
  * @returns {Promise<boolean>}
  */
-function isAppInstalled (app) {
-  return new Promise((resolve) => {
+function isAppInstalled(app) {
+  return new Promise(resolve => {
     if (!(app in prefixes)) {
-      return resolve(false)
+      return resolve(false);
     }
 
     Linking.canOpenURL(prefixes[app])
-      .then((result) => {
-        resolve(!!result)
+      .then(result => {
+        resolve(!!result);
       })
-      .catch(() => resolve(false))
-  })
+      .catch(() => resolve(false));
+  });
 }
 
 /**
@@ -51,17 +56,17 @@ function isAppInstalled (app) {
  * @returns {boolean}
  */
 function isSupportedApp(app) {
-  return Object.keys(titles).includes(app)
+  return Object.keys(titles).includes(app);
 }
 
 /**
  * Get a list of not supported apps from a given array of apps
  *
  * @param {array} apps
- * @returns {array} 
+ * @returns {array}
  */
 function getNotSupportedApps(apps) {
-  return apps.filter(app => !isSupportedApp(app))
+  return apps.filter(app => !isSupportedApp(app));
 }
 
 /**
@@ -70,11 +75,13 @@ function getNotSupportedApps(apps) {
  * @param {array} apps
  */
 export function checkNotSupportedApps(apps) {
-  let notSupportedApps = getNotSupportedApps(apps)
+  let notSupportedApps = getNotSupportedApps(apps);
   if (notSupportedApps.length) {
     throw new MapsException(
-      `appsWhiteList [${notSupportedApps}] are not supported apps, please provide some of the supported apps [${Object.keys(titles)}]`
-    )
+      `appsWhiteList [${notSupportedApps}] are not supported apps, please provide some of the supported apps [${Object.keys(
+        titles
+      )}]`
+    );
   }
 }
 
@@ -88,42 +95,44 @@ export function checkNotSupportedApps(apps) {
  * }} options
  * @returns {Promise}
  */
-export function askAppChoice ({dialogTitle, dialogMessage, cancelText, appsWhiteList}) {
-  return new Promise(async (resolve) => {
-    let availableApps = await getAvailableApps()
+export function askAppChoice({ dialogTitle, dialogMessage, cancelText, appsWhiteList }) {
+  return new Promise(async resolve => {
+    let availableApps = await getAvailableApps();
 
     if (appsWhiteList && appsWhiteList.length) {
-      availableApps = availableApps
-        .filter(appName => appsWhiteList.includes(appName))
+      availableApps = availableApps.filter(appName => appsWhiteList.includes(appName));
     }
 
     if (availableApps.length < 2) {
-      return resolve(availableApps[0] || null)
+      return resolve(availableApps[0] || null);
     }
 
     if (isIOS) {
-      let options = availableApps.map((app) => titles[app])
-      options.push(cancelText)
+      let options = availableApps.map(app => titles[app]);
+      options.push(cancelText);
 
-      ActionSheetIOS.showActionSheetWithOptions({
-        title: dialogTitle,
-        message: dialogMessage,
-        options: options,
-        cancelButtonIndex: options.length - 1
-      }, (buttonIndex) => {
-        if (buttonIndex === options.length - 1) {
-          return resolve(null)
+      ActionSheetIOS.showActionSheetWithOptions(
+        {
+          title: dialogTitle,
+          message: dialogMessage,
+          options: options,
+          cancelButtonIndex: options.length - 1
+        },
+        buttonIndex => {
+          if (buttonIndex === options.length - 1) {
+            return resolve(null);
+          }
+          return resolve(availableApps[buttonIndex]);
         }
-        return resolve(availableApps[buttonIndex])
-      })
+      );
 
-      return
+      return;
     }
 
-    let options = availableApps.map((app) => ({text: titles[app], onPress: () => resolve(app)}))
-    options.push({text: cancelText, onPress: () => resolve(null), style: 'cancel'})
-    return Alert.alert(dialogTitle, dialogMessage, options, {onDismiss: () => resolve(null)})
-  })
+    let options = availableApps.map(app => ({ text: titles[app], onPress: () => resolve(app) }));
+    options.push({ text: cancelText, onPress: () => resolve(null), style: "cancel" });
+    return Alert.alert(dialogTitle, dialogMessage, options, { onDismiss: () => resolve(null) });
+  });
 }
 
 /**
@@ -143,33 +152,37 @@ export function askAppChoice ({dialogTitle, dialogMessage, cancelText, appsWhite
  *     cancelText: string | undefined | null
  * }} options
  */
-export function checkOptions (options) {
-  if (!options || typeof options !== 'object') {
-    throw new MapsException('First parameter of `showLocation` should contain object with options.')
+export function checkOptions(options) {
+  if (!options || typeof options !== "object") {
+    throw new MapsException("First parameter of `showLocation` should contain object with options.");
   }
-  if (!('latitude' in options) || !('longitude' in options)) {
-    throw new MapsException('First parameter of `showLocation` should contain object with at least keys `latitude` and `longitude`.')
+  if (!("latitude" in options) || !("longitude" in options)) {
+    throw new MapsException(
+      "First parameter of `showLocation` should contain object with at least keys `latitude` and `longitude`."
+    );
   }
-  if ('title' in options && options.title && typeof options.title !== 'string') {
-    throw new MapsException('Option `title` should be of type `string`.')
+  if ("title" in options && options.title && typeof options.title !== "string") {
+    throw new MapsException("Option `title` should be of type `string`.");
   }
-  if ('googleForceLatLon' in options && options.googleForceLatLon && typeof options.googleForceLatLon !== 'boolean') {
-    throw new MapsException('Option `googleForceLatLon` should be of type `boolean`.')
+  if ("googleForceLatLon" in options && options.googleForceLatLon && typeof options.googleForceLatLon !== "boolean") {
+    throw new MapsException("Option `googleForceLatLon` should be of type `boolean`.");
   }
-  if ('googlePlaceId' in options && options.googlePlaceId && typeof options.googlePlaceId !== 'string') {
-    throw new MapsException('Option `googlePlaceId` should be of type `string`.')
+  if ("googlePlaceId" in options && options.googlePlaceId && typeof options.googlePlaceId !== "string") {
+    throw new MapsException("Option `googlePlaceId` should be of type `string`.");
   }
-  if ('app' in options && options.app && !(options.app in prefixes)) {
-    throw new MapsException('Option `app` should be undefined, null, or one of the following: "' + Object.keys(prefixes).join('", "') + '".')
+  if ("app" in options && options.app && !(options.app in prefixes)) {
+    throw new MapsException(
+      'Option `app` should be undefined, null, or one of the following: "' + Object.keys(prefixes).join('", "') + '".'
+    );
   }
-  if ('appsWhiteList' in options && options.appsWhiteList) {
-    checkNotSupportedApps(options.appsWhiteList)
+  if ("appsWhiteList" in options && options.appsWhiteList) {
+    checkNotSupportedApps(options.appsWhiteList);
   }
 }
 
 class MapsException {
-  constructor (message) {
-    this.message = message
-    this.name = 'MapsException'
+  constructor(message) {
+    this.message = message;
+    this.name = "MapsException";
   }
 }
