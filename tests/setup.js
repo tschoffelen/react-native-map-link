a1c0add9fcef38defe6fc7ae8d3637855e4dbf5d
Tomasz Lorenc <lorenc.tomasz@gmail.com>
2023-05-30 11:43:06 +0200

    Mock dependency

--- tests/setup.js
+++ tests/setup.js
@@ -1,5 +1,17 @@
+import Share from 'react-native-share';
+
 jest.mock('react-native/Libraries/Linking/Linking', () => ({
   canOpenURL: jest.fn((url) => {
     return Promise.resolve(url.includes('https://') || url.includes('uber://'));
+  }),
+}));
+
+jest.mock('react-native-share', () => ({
+  isPackageInstalled: jest.fn((appPackageName) => {
+    const isAppInstalled = appPackageName === 'com.tranzmate';
+    return Promise.resolve({
+      isInstalled: isAppInstalled,
+      message: isAppInstalled ? 'Installed' : 'Not installed',
+    });
   }),
 }));
