ed3b4bb07fc00ca7b4356fb8e6ab61386e6fc844
Fabian Urrutia <fabian.urrutia@live.cl>
2024-02-12 22:05:09 -0300

    Fix tests

--- tests/setup.js
+++ tests/setup.js
@@ -1,6 +1,6 @@
 jest.mock('react-native', () => ({
   Linking: {
-    openURL: jest.fn(),
+    openURL: jest.fn(() => Promise.resolve('mockResolve')),
     canOpenURL: jest.fn((url) => {
       return Promise.resolve(
         url.includes('https://') || url.includes('uber://'),
